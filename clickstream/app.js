74d31abf446441d1aa9ec66191b9dbd24f8185a9
Mac Book Air <macbookair@MacBook-Air.local>
2020-03-26 19:24:27 -0300

    Funionando heatmap

--- clickstream/app.js
+++ clickstream/app.js
@@ -14,4 +14,21 @@ const trackClickStream = () => {
   document.addEventListener("click", pushEventData);
   return data;
 };
-trackClickStream();
+
+
+const paintLive = (data, max = 5)=>{
+    const heatmap = h337.create({
+        container: document.documentElement
+    })
+    const update = () =>{
+        heatmap.setData({
+            max,
+            data
+        })
+    }
+    setInterval(()=>{
+        update(data)
+    }, 10)
+}
+const data = trackClickStream();
+paintLive(data);
