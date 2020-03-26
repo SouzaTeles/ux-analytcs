f929ab2e7f90131e94da7b06dc7edf342a381201
Mac Book Air <macbookair@MacBook-Air.local>
2020-03-26 19:43:56 -0300

    nova rota

--- server/api.js
+++ server/api.js
@@ -1,20 +1,32 @@
 const http = require("http");
 const fs = require("fs");
+const path = require("path");
 
-function api_get(req, res){
-    fs.readdir(`${__dirname}/data/`, (err, files)=>{
-        if(err) throw err;
-        res.write(JSON.stringify(files));
-        res.end();
-        // console.log(files)
-    })
+const DATA_DIR = path.join(__dirname, "data");
+function api_get(req, res) {
+  fs.readdir(DATA_DIR, (err, files) => {
+    if (err) throw err;
+    const filesNo = files.map(f => f.replace(".json", ""));
+    res.write(JSON.stringify(filesNo));
+    res.end();
+    // console.log(files)
+  });
 }
 
-function handleServer(req, res){
-    console.log(req.url);
-    if(req.url === "/api/"){
-        // res.end("funcionando")
-        api_get(req, res);
-    }
+function api_id_get(req, res) {
+    const file
+    res.write("funcionou");
+    res.end();
 }
-http.createServer(handleServer).listen(3001)
+
+function handleServer(req, res) {
+  console.log(req.url);
+  if (req.url === "/api/") {
+    // res.end("funcionando")
+    api_get(req, res);
+  }
+  else if(req.url.indexOf("/api/") > -1 && req.method == "GET") {
+      api_id_get(req, res);
+  }
+}
+http.createServer(handleServer).listen(3001);
