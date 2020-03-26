a0896cabc9fb2679acbc3566829a3cea77f31130
Mac Book Air <macbookair@MacBook-Air.local>
2020-03-26 19:35:33 -0300

    listando os arquivos

--- server/api.js
+++ server/api.js
@@ -1,7 +1,19 @@
 const http = require("http");
 const fs = require("fs");
 
-const handleServer(req, res){
-    res.end("funcionando")
+function api_get(req, res){
+    fs.readdir(`${__dirname}/data/`, (err, files)=>{
+        if(err) throw err;
+        res.write(JSON.stringify(files));
+        // console.log(files)
+    })
+}
+
+function handleServer(req, res){
+    console.log(req.url);
+    if(req.url === "/api/"){
+        // res.end("funcionando")
+        api_get(req, res);
+    }
 }
 http.createServer(handleServer).listen(3001)
