33575d7a2c22afc55c4689abeb497cbc38da4d09
Mac Book Air <macbookair@MacBook-Air.local>
2020-03-26 20:02:32 -0300

    Post

--- server/api.js
+++ server/api.js
@@ -22,22 +22,38 @@ function api_id_get(req, res) {
   fs.readFile(file, (err, json) => {
     if (err) throw err;
     else if (json) {
-        console.log('antes')
-        console.log(json.toString())
-        console.log('depois')
+      console.log("antes");
+      console.log(json.toString());
+      console.log("depois");
       res.write(json.toString());
       res.end();
     }
   });
 }
 
+function api_id_post(req, res) {
+  const fileName = req.url.split("?name=");
+  console.log(fileName)
+  res.write('aaa')
+  res.end();
+}
+
 function handleServer(req, res) {
+  res.writeHeader(200, {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Content-Type": "application/json"
+  });
+
   console.log(req.url);
   if (req.url === "/api/") {
     // res.end("funcionando")
     api_get(req, res);
   } else if (req.url.indexOf("/api/") > -1 && req.method == "GET") {
     api_id_get(req, res);
+  }
+  else if (req.url.indexOf("/api/") > -1 && req.method == "POST") {
+    api_id_post(req, res);
   }
 }
 http.createServer(handleServer).listen(3001);
